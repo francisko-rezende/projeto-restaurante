0cd710052e123f3f20bf8cde6821e9450a3d6f0d
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-08-12 20:03:02 -0300

    termina "[M1S07] - Ex 2 - Criar Navbar"

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Navbar = () => {
+  return <header>
+    <h1>
+      <img src="/fish.svg" alt="Restaurante da Marilene" />
+    </h1>
+    <h2>Menu</h2>
+  </header>;
+};
