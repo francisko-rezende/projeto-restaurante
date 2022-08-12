5ca76061f41b331fa09bfa118df8ec3c8d787376
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-08-12 20:32:44 -0300

    termina "[M1S07] - Ex 3 - Criar Footer"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { Footer } from './Components/Footer'
 import { Navbar } from './Components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   )
 }
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <address>
+        <ul>
+          <li>Praia de Jurere - Canasvieiras, Florianópolis - SC</li>
+          <li>
+            <ul>
+              <li><a href="#"><img src="/whatsapp.svg" alt="Entre em contato por ZapZoperson"/></a></li>
+              <li><a href="#"><img src="/instagram.svg" alt="Visite nosso Insta"/></a></li>
+            </ul>
+          </li>
+        </ul>
+      </address>
+      <small>© Marilene—Alguns direitos reservados</small>
+    </footer>
+  );
+};
